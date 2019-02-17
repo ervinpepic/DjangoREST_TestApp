dcb115770e5f7e6e1f33fef5ef1578d0bb516642
Ervin Pepic <ervin@Ervins-MacBook-Pro-2.local>
2019-02-18 00:02:51 +0100

    Switched to full generic APIView

--- src/snippets/views.py
+++ src/snippets/views.py
@@ -1,4 +1,3 @@
-from rest_framework import mixins
 from rest_framework import generics
 
 from snippets.models import Snippet
@@ -7,32 +6,14 @@ from snippets.serializers import SnippetSerializer
 
 # Create your views here.
 
-class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
-	"""
-		List all snippets or create a new snippet.
-	"""
+class SnippetList(generics.ListCreateAPIView):
 	queryset 			= Snippet.objects.all()
 	serializer_class 	= SnippetSerializer
 
-	def get(self, request, *args, **kwargs):
-		return self.list(request, *args, **kwargs)
 
-	def post(self, request, *args, **kwargs):
-		return self.create(request, *args, **kwargs)
-class SnippetDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
-	"""
-		Retrieve, Update or Delete snippet instance.
-	"""
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 	queryset 			= Snippet.objects.all()
 	serializer_class 	= SnippetSerializer
 
-	def get(self, request, *args, **kwargs):
-		return self.retrieve(request, *args, **kwargs)
-
-	def put(self, request, *args, **kwargs):
-		return self.update(request, *args, **kwargs)
-
-	def delete(self, request, *args, **kwargs):
-		return self.delete(request, *args, **kwargs)
 
 		
