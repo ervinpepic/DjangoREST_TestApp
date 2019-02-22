ad1a6bed7507f2befaff66dc51a908978b1c94e8
Ervin Pepic <ervin@Ervins-MacBook-Pro-2.local>
2019-02-22 23:31:02 +0100

    Added HyperLinked Relation ship between entities

--- src/snippets/serializers.py
+++ src/snippets/serializers.py
@@ -4,11 +4,13 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class SnippetSerializer(serializers.ModelSerializer):
-	owner 	= serializers.ReadOnlyField(source='owner.username')
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+	owner 		= serializers.ReadOnlyField(source='owner.username')
+	highlight	= serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
 	class Meta:
 		model 	= Snippet		
-		fields 	= ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
+		fields 	= ('url', 'id', 'highlight', 'owner', 'title', 'code', 'linenos', 'language', 'style')
 
 		
 	def create(self, validated_data):
@@ -32,10 +34,10 @@ class SnippetSerializer(serializers.ModelSerializer):
 
 		return instance
 
-class UserSerializer(serializers.ModelSerializer):
-	snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+	snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
 	class Meta:
 		model 	= User
-		fields 	= ('id', 'username', 'snippets')
+		fields 	= ('url', 'id', 'username', 'snippets')
 
