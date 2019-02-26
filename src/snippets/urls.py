fe7b736fdcb12f7e0f1cf0cbad409e679ed77e12
Ervin Pepic <ervin@Ervins-MacBook-Pro-2.local>
2019-02-26 23:30:50 +0100

    Added auto schema client documentation

--- src/snippets/urls.py
+++ src/snippets/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 
 from snippets import views
+
 
 
 router = DefaultRouter()
@@ -11,10 +13,13 @@ router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
 
+schema_view = get_schema_view(title='Pastebin API')
+	
 
 #The API urls are now determined automaticaly by the router
 urlpatterns = [
 	path('', include(router.urls)),
+	path('schema/', schema_view),
 ]
 
 
