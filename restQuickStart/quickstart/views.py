497ca593f747659b69eabcb992c88903481680c9
Ervin Pepic <ervin@Ervins-MacBook-Pro-2.local>
2019-02-28 00:10:23 +0100

    Finshed quickstart, added serializer model for user and group BuiltIN classes

--- restQuickStart/quickstart/views.py
+++ restQuickStart/quickstart/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
 
-# Create your views here.
+from rest_framework import viewsets
+
+from quickstart.serializers import UserSerializer, GroupSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+	"""
+		API endpoints that allows user to be viewed or edited.
+	"""
+	queryset			= User.objects.all().order_by('-date_joined')
+	serializer_class 	= UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+	"""
+		API endpints that allow groups to be viewed or edited.
+	"""
+
+	queryset 			= Group.objects.all()
+	serializer_class	= GroupSerializer
